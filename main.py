ad3d2fc20e5ed1443713369e0c5559e977565a87
goncalogomes <gonndr@gmail.com>
2022-05-06 16:42:31 +0100

    updated main

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
 from instagram_bot import InstagramBot
 
-insta_bot = InstagramBot()
+def main():
+    insta_bot = InstagramBot()
 
-handle = insta_bot.login()
+    handle = insta_bot.login()
 
-followers = insta_bot.find_followers(handle)
+    followers = insta_bot.find_followers(handle)
 
-following = insta_bot.find_following(handle)
+    following = insta_bot.find_following(handle)
 
-print("Followers: ", len(followers))
-print("Following: ", len(following))
+    print("Followers: ", len(followers))
+    print("Following: ", len(following))
 
-# common_people = list(set(following).intersection(followers))
+    # common_people = list(set(following).intersection(followers))
 
-people_not_following_back = list(set(following)-set(followers))
+    people_not_following_back = list(set(following)-set(followers))
 
-print(people_not_following_back)
+    print(people_not_following_back)
 
-with open("people_not_following_back.txt", "w") as file:
-    for person in people_not_following_back:
-        file.write(person + "\n")
+    with open("people_not_following_back.txt", "w") as file:
+        for person in people_not_following_back:
+            file.write(person + "\n")
+
+
+if __name__ == "__main__":
+    main()
